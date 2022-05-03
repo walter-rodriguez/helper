df6382342656dba6413f2ea1f449477385a41adf
Walter Fabian Rodriguez Salazar <walterfrs@outlook.com>
2022-05-02 22:31:33 -0500

    Correccion de tipado

--- slice/slice.go
+++ slice/slice.go
@@ -2,7 +2,7 @@ package slice
 
 // Busca la posicion de un slice de cualquier tipo con la función de comparación `validate`
 // Retorna la posicion del slice si existe, si no existe o no se encuentra devuelve -1
-func SliceIndex(limit int, validate func(int) bool) int {
+func Index(limit int, validate func(int) bool) int {
 	for i := 0; i < limit; i++ {
 		if validate(i) {
 			return i
